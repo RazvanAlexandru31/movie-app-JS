2bf582bea22484656c75422c71cfa11264c27f13
RazvanAlexandru31 <razvan.donita@gmail.com>
2022-11-13 16:59:23 +0200

    project done

---
+++ README.md
@@ -0,0 +1,9 @@
+#Movie App JS
+
+Movie Search App using OMDb API(Developed with HTML5, CSS3 and JS).The user is able to search for a movies and view some general information about them.
+
+
+![screenshot1](./screenshots/movie-app-1.png)
+![screenshot2](./screenshots/movie-app-2.png)
+![screenshot3](./screenshots/movie-app-3.png)
+![screenshot4](./screenshots/movie-app-4.png)
--- script.js
+++ script.js
@@ -148,3 +148,29 @@ const showMovieCard = (movie) => {
       </div>
   `;
 };
+
+// Swiper JS
+const swiper = new Swiper('.swiper', {
+  // Optional parameters
+  direction: 'vertical',
+  loop: true,
+  autoplay: {
+    delay: 2500,
+    disableOnInteraction: false,
+  },
+  // If we need pagination
+  pagination: {
+    el: '.swiper-pagination',
+  },
+
+  // Navigation arrows
+  navigation: {
+    nextEl: '.swiper-button-next',
+    prevEl: '.swiper-button-prev',
+  },
+
+  // And if we need scrollbar
+  scrollbar: {
+    el: '.swiper-scrollbar',
+  },
+});
